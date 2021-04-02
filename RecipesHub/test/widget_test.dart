f37b5a2853d03840abc7ecc2b2de6992455df1c8
David1701520471 <67935842+David1701520471@users.noreply.github.com>
2021-04-02 18:25:39 -0500

    testConflict

--- RecipesHub/test/widget_test.dart
+++ RecipesHub/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'file:///C:/Users/Usuario/AndroidStudioProjects/flutter_app/lib/Pages/main.dart';
+import 'file:///E:/ProyectoS3RH/RecipesHub/RecipesHub/lib/Pages/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
