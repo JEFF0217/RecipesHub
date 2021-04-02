0afcbd551a2418cf4bb8bee1004bbb6049e0ac51
William1701520853 <52585477+William1701520853@users.noreply.github.com>
2021-04-02 18:20:16 -0500

    Update widget_test.dart

--- RecipesHub/test/widget_test.dart
+++ RecipesHub/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'file:///C:/Users/Usuario/AndroidStudioProjects/flutter_app/lib/Pages/main.dart';
+import 'file:///F:/Escritorio/Octavo/Software 3/ProyectoMovil/RecipesHub/RecipesHub/lib/Pages/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
